46381112fda3df0a8078d818b7a16488987e2cb6
IMPWNG <matisse.ach+github.com>
2022-05-05 10:09:22 -0400

    landmarks+id

--- venv/PoseEstimationMin.py
+++ venv/PoseEstimationMin.py
@@ -1,19 +1,26 @@
+from unittest import result
 import cv2
 import mediapipe as mp
 import time
 
-
-mpPose = mp.solutions.po.setdefault
+mpDraw = mp.solutions.drawing_utils
+mpPose = mp.solutions.pose
 pose = mpPose.Pose()
 
 
 cap = cv2.VideoCapture('Video/1vid.mp4')
 
 pTime = 0
-ctime = 0
 
 while True:
     success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = pose.process(imgRGB) 
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        for id, lm in enumerate(results.pose_landmarks.landmark):
+            h, w, c = img.shape
+            print (id, lm)
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
