74ffe32e99e4c2585fdb5488f5dcb90ac3625335
IMPWNG <matisse.ach+github.com>
2022-05-06 15:29:04 -0400

    Min.py✅

--- venv/PoseEstimationMin.py
+++ venv/PoseEstimationMin.py
@@ -8,7 +8,7 @@ mpPose = mp.solutions.pose
 pose = mpPose.Pose()
 
 
-cap = cv2.VideoCapture('Video/1vid.mp4')
+cap = cv2.VideoCapture('Video/4vid.mp4')
 
 pTime = 0
 
@@ -21,6 +21,9 @@ while True:
         for id, lm in enumerate(results.pose_landmarks.landmark):
             h, w, c = img.shape
             print (id, lm)
+            cx, cy = int(lm.x * w), int(lm.y * h)
+            cv2.circle(img, (cx,cy), 5, (255,0,0), cv2.FILLED)
+            
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
@@ -28,6 +31,6 @@ while True:
     
     cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,0), 3)
     
-    cv2.imshow("Image", img)
+    cv2.imshow("Image", img) 
     cv2.waitKey(1)
 
